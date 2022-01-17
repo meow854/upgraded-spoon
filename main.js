ad57cbd15c47dbb00e163893b19e709ad62147bf
meow854 <81199224+meow854@users.noreply.github.com>
2022-01-17 10:07:35 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+//https://teachablemachine.withgoogle.com/models/OTIvlQtuw/
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera= document.getElementById("camera");
+Webcam.attach('#camera');
+
+function takepic() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML= "<img src='" + data_uri + "' id='takenimg'>";
+    });
+}
+
+console.log("ml5 version", ml5.version);
+classifier= ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/OTIvlQtuw/model.json", modelLoaded);
+
+function modelLoaded() {
+    console.log("model loaded");
+}
